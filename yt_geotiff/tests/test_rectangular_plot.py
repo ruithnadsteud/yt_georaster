f480aff77c87989afb82cb89b295c721b8693307
arevill2 <43847496+arevill2@users.noreply.github.com>
2021-02-26 00:11:01 +0000

    test selectors, plot and exceed image dims

--- yt_geotiff/tests/test_rectangular_plot.py
+++ yt_geotiff/tests/test_rectangular_plot.py
@@ -8,7 +8,7 @@ from yt_geotiff.testing import requires_file
 land_use_data = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
 
 @requires_file(land_use_data)
-def test_rectangular():
+def test_plot():
     ds = yt.load(land_use_data)
     width = ds.arr(2000., 'm')
     height = ds.arr(2000.,'m') 
