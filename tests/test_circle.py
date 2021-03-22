1c569dd8d19dc444bdf8dd49778c83296eed71ce
Britton Smith <brittonsmith@gmail.com>
2021-03-22 10:56:43 +0000

    Check data container type in circle test.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -2,7 +2,8 @@ import yt
 import yt.extensions.geotiff
 import yt.geometry.selection_routines as selector_shape
 
-from yt_geotiff.data_structures import GeoTiffDataset
+from yt.data_objects.selection_objects.spheroids import YTSphere
+
 from yt_geotiff.testing import requires_file
 
 land_use_data = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
@@ -14,4 +15,4 @@ def test_circle():
     circle_centre = ds.arr([3501279,3725080],'m')
     circular_yt_container = ds.circle(circle_centre, radius)
     circular_yt_container[('bands','1')]
-    assert isinstance(circular_yt_container.selector,selector_shape.SphereSelector)
+    assert isinstance(circular_yt_container, YTSphere)
