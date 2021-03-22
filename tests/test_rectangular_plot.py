ad0381e141c62005d45042555c02d298d8da169b
Britton Smith <brittonsmith@gmail.com>
2021-03-22 10:58:23 +0000

    Remove imports.

--- tests/test_rectangular_plot.py
+++ tests/test_rectangular_plot.py
@@ -1,8 +1,6 @@
 import yt
 import yt.extensions.geotiff
-import yt.geometry.selection_routines as selector_shape
 
-from yt_geotiff.data_structures import GeoTiffDataset
 from yt_geotiff.testing import requires_file
 
 land_use_data = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
@@ -11,7 +9,7 @@ land_use_data = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
 def test_plot():
     ds = yt.load(land_use_data)
     width = ds.arr(2000., 'm')
-    height = ds.arr(2000.,'m') 
+    height = ds.arr(2000.,'m')
     rectangle_centre = ds.arr([3501279,3725080],'m')
 
     p = ds.plot(('bands', '1'), height=height, width=width, center=rectangle_centre)
