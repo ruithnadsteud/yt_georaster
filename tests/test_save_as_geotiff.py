0eedc09020b31ed9cf1e4529dde70847ccd13b16
Britton Smith <brittonsmith@gmail.com>
2021-05-05 16:28:12 +0100

    Uncomment test.

--- tests/test_save_as_geotiff.py
+++ tests/test_save_as_geotiff.py
@@ -15,23 +15,23 @@ S2_dir = "M2_Sentinel-2_test_data"
 LS_dir = "Landsat-8_sample_L2"
 
 class GeoTiffSaveTest(TempDirTest):
-    # @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
-    # @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
-    # def test_save_default(self):
-    #     fns = glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2")) + \
-    #       glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF"))
+    @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
+    @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
+    def test_save_default(self):
+        fns = glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2")) + \
+          glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF"))
 
-    #     ds = yt.load(*fns)
+        ds = yt.load(*fns)
 
-    #     ds_fn, fm_fn = save_as_geotiff(ds, "my_data.tif")
-    #     ds_new = yt.load(ds_fn, field_map=fm_fn)
+        ds_fn, fm_fn = save_as_geotiff(ds, "my_data.tif")
+        ds_new = yt.load(ds_fn, field_map=fm_fn)
 
-    #     for field in ds.field_list:
-    #         ad = ds.all_data()
-    #         ad_new = ds_new.all_data()
-    #         assert_array_equal(
-    #             ad[field], ad_new[field],
-    #             err_msg=f"Saved data mismatch for field {field}.")
+        for field in ds.field_list:
+            ad = ds.all_data()
+            ad_new = ds_new.all_data()
+            assert_array_equal(
+                ad[field], ad_new[field],
+                err_msg=f"Saved data mismatch for field {field}.")
 
     @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
     @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
