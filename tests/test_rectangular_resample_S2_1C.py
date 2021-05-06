e4fade04d2dea66d03bd1f708d63f2cfdc36f3fc
arevill2 <43847496+arevill2@users.noreply.github.com>
2021-05-06 12:16:34 +0100

    test fix v3

--- tests/test_rectangular_resample_S2_1C.py
+++ tests/test_rectangular_resample_S2_1C.py
@@ -5,14 +5,15 @@ from yt.data_objects.selection_objects.region import YTRegion
 
 from yt_geotiff.testing import requires_file
 
-s2_data = "Sentinel-2_sample_L1C/T33UXP_20170501T100031_B02.jp2"
+s2_data_1 = "Sentinel-2_sample_L1C/T36MVE_20210315T075701_B02.jp2"
+s2_data_2 = "Sentinel-2_sample_L1C/T36MVE_20210315T075701_B01.jp2"
 
-@requires_file(s2_data)
+@requires_file(s2_data_1, s2_data_2)
 def test_rectangular():
-    ds = yt.load(s2_data)
-    width = ds.quan(5., 'km')
-    height = ds.quan(5.,'km')
-    rectangle_centre = ds.arr([453725,9974362],'m')
+    ds = yt.load(s2_data_1, s2_data_2)
+    width = ds.quan(20., 'km')
+    height = ds.quan(10.,'km')
+    rectangle_centre = ds.arr([471696,9989860],'m')
     rectangular_yt_container = ds.rectangle_from_center(rectangle_centre,width,height)
     rectangular_yt_container[('bands', 'B01')]
     assert isinstance(rectangular_yt_container, YTRegion)
--- tests/test_rectangular_resample_S2_2A.py
+++ tests/test_rectangular_resample_S2_2A.py
@@ -5,14 +5,15 @@ from yt.data_objects.selection_objects.region import YTRegion
 
 from yt_geotiff.testing import requires_file
 
-s2_data = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B02_20m.jp2"
+s2_data_1 = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B02_20m.jp2"
+s2_data_2 = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B01_60m.jp2"
 
-@requires_file(s2_data)
+@requires_file(s2_data_1, s2_data_2)
 def test_rectangular():
-    ds = yt.load(s2_data)
+    ds = yt.load(s2_data_1, s2_data_2)
     width = ds.quan(500., 'm')
     height = ds.quan(500.,'m')
-    rectangle_centre = ds.arr([488012,6199162],'m')
+    rectangle_centre = ds.arr([473951,6155327],'m')
     rectangular_yt_container = ds.rectangle_from_center(rectangle_centre,width,height)
     rectangular_yt_container[('bands','B01_60m')]
     assert isinstance(rectangular_yt_container, YTRegion)
