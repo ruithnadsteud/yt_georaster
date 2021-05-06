ac351a85a35344c8b4e11d17b60d443c7e66caf7
Britton Smith <brittonsmith@gmail.com>
2021-05-06 16:34:12 +0100

    Fix paths and field names in a couple tests.

--- tests/test_rectangular_resample_S2_1C.py
+++ tests/test_rectangular_resample_S2_1C.py
@@ -3,17 +3,18 @@ import yt.extensions.geotiff
 
 from yt.data_objects.selection_objects.region import YTRegion
 
-from yt_geotiff.testing import requires_file
+from yt_geotiff.testing import get_path, requires_file
 
-s2_data_1 = "Sentinel-2_sample_L1C/T36MVE_20210315T075701_B02.jp2"
-s2_data_2 = "Sentinel-2_sample_L1C/T36MVE_20210315T075701_B01.jp2"
+s2_data_1 = "M2_Sentinel-2_test_data/T36MVE_20210315T075701_B02.jp2"
+s2_data_2 = "M2_Sentinel-2_test_data/T36MVE_20210315T075701_B01.jp2"
 @requires_file(s2_data_1)
 @requires_file(s2_data_2)
 def test_rectangular():
-    ds = yt.load(s2_data_1, s2_data_2)
+    fns = get_path([s2_data_1, s2_data_2])
+    ds = yt.load(*fns)
     width = ds.quan(20., 'km')
     height = ds.quan(10.,'km')
     rectangle_centre = ds.arr([471696,9989860],'m')
     rectangular_yt_container = ds.rectangle_from_center(rectangle_centre,width,height)
-    rectangular_yt_container[('bands', 'B01')]
+    rectangular_yt_container[('bands', 'S2_B01')]
     assert isinstance(rectangular_yt_container, YTRegion)
--- tests/test_rectangular_resample_S2_2A.py
+++ tests/test_rectangular_resample_S2_2A.py
@@ -1,19 +1,21 @@
+import os
 import yt
 import yt.extensions.geotiff
 
 from yt.data_objects.selection_objects.region import YTRegion
 
-from yt_geotiff.testing import requires_file
+from yt_geotiff.testing import get_path, requires_file
 
 s2_data_1 = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B02_20m.jp2"
 s2_data_2 = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B01_60m.jp2"
 @requires_file(s2_data_1)
 @requires_file(s2_data_2)
 def test_rectangular():
-    ds = yt.load(s2_data_1, s2_data_2)
+    fns = get_path([s2_data_1, s2_data_2])
+    ds = yt.load(*fns)
     width = ds.quan(500., 'm')
     height = ds.quan(500.,'m')
     rectangle_centre = ds.arr([473951,6155327],'m')
     rectangular_yt_container = ds.rectangle_from_center(rectangle_centre,width,height)
-    rectangular_yt_container[('bands','B01_60m')]
+    rectangular_yt_container[('bands','S2_B01')]
     assert isinstance(rectangular_yt_container, YTRegion)
