e8b0d3bdc505aff4073fd968a253e330da567556
Britton Smith <brittonsmith@gmail.com>
2021-05-26 16:31:39 +0100

    Add cython polygon selector.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
 
 def get_version(filename):
     """
@@ -17,6 +20,42 @@ VERSION = get_version("yt_geotiff/__init__.py")
 
 dev_requirements = [
     'flake8', 'pytest>=3.6']
+
+std_libs = []
+cython_extensions = [
+    Extension("yt_geotiff.polygon_selector",
+              ["yt_geotiff/polygon_selector.pyx"],
+              libraries=std_libs),
+]
+
+class build_ext(_build_ext):
+    # subclass setuptools extension builder to avoid importing cython and numpy
+    # at top level in setup.py. See http://stackoverflow.com/a/21621689/1382869
+    def finalize_options(self):
+        from Cython.Build import cythonize
+        self.distribution.ext_modules[:] = cythonize(
+                self.distribution.ext_modules)
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process
+        # see http://stackoverflow.com/a/21621493/1382869
+        if isinstance(__builtins__, dict):
+            # sometimes this is a dict so we need to check for that
+            # https://docs.python.org/3/library/builtins.html
+            __builtins__["__NUMPY_SETUP__"] = False
+        else:
+            __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+class sdist(_sdist):
+    # subclass setuptools source distribution builder to ensure cython
+    # generated C files are included in source distribution.
+    # See http://stackoverflow.com/a/18418524/1382869
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are up-to-date
+        from Cython.Build import cythonize
+        cythonize(cython_extensions)
+        _sdist.run(self)
 
 setup(name="yt_geotiff",
       version=VERSION,
@@ -48,5 +87,7 @@ setup(name="yt_geotiff",
       extras_require={
           'dev': dev_requirements,
       },
+      cmdclass={'sdist': sdist, 'build_ext': build_ext},
+      ext_modules=cython_extensions,
       python_requires='>=3.6'
       )
