0f1e4d5851f203925b4040f6ca6604d275c186d5
Britton Smith <brittonsmith@gmail.com>
2021-06-10 14:40:32 +0100

    Add plot tests.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -15,71 +15,55 @@ S2_dir = "M2_Sentinel-2_test_data"
 LS_dir = "Landsat-8_sample_L2"
 poly_multi = "example_multi-feature_polygon/multi_feature_polygon.shp"
 
-class GeoRasterSaveTest(TempDirTest):
+class GeoRasterPlotTest(TempDirTest):
     @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
     @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
-    def test_save_default(self):
+    def test_plot_default(self):
         fns = glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2")) + \
           glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF"))
 
         ds = yt.load(*fns)
 
-        ds_fn, fm_fn = save_as_geotiff(ds, "my_data.tif")
-        ds_new = yt.load(ds_fn, field_map=fm_fn)
-
-        for field in ds.field_list:
-            ad = ds.all_data()
-            ad_new = ds_new.all_data()
-            assert_array_equal(
-                ad[field], ad_new[field],
-                err_msg=f"Saved data mismatch for field {field}.")
-
-    @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
-    @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
-    def test_save_circle(self):
-        fns = glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2")) + \
-          glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF"))
-
-        ds = yt.load(*fns)
-
-        circle = ds.circle(ds.domain_center, (10, 'km'))
         fields = [("bands", "LS_B1_30m"),
                   ("bands", "S2_B06_20m"),
                   ("band_ratios", "S2_NDWI"),
                   ("variables", "LS_temperature")]
-        ds_fn, fm_fn = save_as_geotiff(
-            ds, "my_data.tiff",
-            fields=fields, data_source=circle)
-        ds_new = yt.load(ds_fn, field_map=fm_fn)
-
-        circle_new = ds_new.circle(circle.center, circle.radius)
 
         for field in fields:
-            assert_array_equal(
-                circle[field], circle_new[field],
-                err_msg=f"Saved data mismatch for field {field}.")
+            p = ds.plot(field)
+            p.save()
+
+    @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
+    @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
+    def test_plot_circle(self):
+        fns = glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2")) + \
+          glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF"))
+
+        ds = yt.load(*fns)
+
+        fields = [("bands", "LS_B1_30m"),
+                  ("bands", "S2_B06_20m"),
+                  ("band_ratios", "S2_NDWI"),
+                  ("variables", "LS_temperature")]
+
+        circle = ds.circle(ds.domain_center, 0.25 * ds.domain_width[:2].min())
+        for field in fields:
+            p = ds.plot(field, data_source=circle)
+            p.save()
 
     @requires_file(poly_multi)
     @requires_file(os.path.join(S2_dir, "T36MVE_20210315T075701_B01.jp2"))
     @requires_file(os.path.join(LS_dir, "LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"))
-    def test_save_polygon(self):
+    def test_plot_polygon(self):
         fns = glob.glob(os.path.join(test_data_dir, LS_dir, "*.TIF")) + \
           glob.glob(os.path.join(test_data_dir, S2_dir, "*.jp2"))
 
         ds = yt.load(*fns)
 
-        polygon = ds.polygon(os.path.join(test_data_dir, poly_multi))
         fields = [("bands", "LS_B1_30m"),
-                  ("bands", "S2_B06_20m"),
                   ("variables", "LS_temperature")]
-        ds_fn, fm_fn = save_as_geotiff(
-            ds, "my_data.tiff",
-            fields=fields, data_source=polygon)
-        ds_new = yt.load(ds_fn, field_map=fm_fn)
 
-        polygon_new = ds_new.polygon(os.path.join(test_data_dir, poly_multi))
-
+        polygon = ds.polygon(os.path.join(test_data_dir, poly_multi))
         for field in fields:
-            assert_array_equal(
-                polygon[field], polygon_new[field],
-                err_msg=f"Saved data mismatch for field {field}.")
+            p = ds.plot(field, data_source=polygon)
+            p.save()
