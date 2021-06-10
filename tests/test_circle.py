3ebae0e3070953d5772848bb4f7abc7ad8659d32
Britton Smith <brittonsmith@gmail.com>
2021-06-10 10:30:32 +0100

    New circle tests.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,38 +1,80 @@
+import glob
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_equal
+import os
 import yt
 import yt.extensions.geotiff
-import yt.geometry.selection_routines as selector_shape
 
-from yt.data_objects.selection_objects.spheroids import YTSphere
+from yt.config import ytcfg
 
 from yt_geotiff.testing import requires_file
 
-land_use_data = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
-s2l1c_data = "Sentinel-2_sample_L1C/T33UXP_20170501T100031_B02.jp2"
-s2l2a_data = "Sentinel-2_sample_L2A/T30UVG_20200601T113331_B02_20m.jp2"
+test_data_dir = ytcfg.get("yt", "test_data_dir")
+landuse = "200km_2p5m_N38E34/200km_2p5m_N38E34.TIF"
+landsat = "Landsat-8_sample_L2/LC08_L2SP_171060_20210227_20210304_02_T1_SR_B1.TIF"
+s2 = "M2_Sentinel-2_test_data/T36MVE_20210315T075701_B01.jp2"
 
-@requires_file(land_use_data)
-def test_circle():
-    ds = yt.load(land_use_data)
-    radius = ds.quan(1000.,'m')
-    circle_centre = ds.arr([3501279,3725080],'m')
-    circular_yt_container = ds.circle(circle_centre, radius)
-    circular_yt_container[('bands','200km_2p5m_N38E34_1')]
-    assert isinstance(circular_yt_container, YTSphere)
+landsat_fns = glob.glob(os.path.join(test_data_dir, os.path.dirname(landsat), "*.TIF"))
+s2_fns = glob.glob(os.path.join(test_data_dir, os.path.dirname(s2), "*.jp2"))
 
-@requires_file(s2l1c_data)
-def test_circle_S2L1C():
-    ds = yt.load(s2l1c_data)
-    radius = ds.quan(5.,'km')
-    circle_centre = ds.arr([453725,9974362],'m')
-    circular_yt_container = ds.circle(circle_centre, radius)
-    circular_yt_container[('bands', 'S2_B02')]
-    assert isinstance(circular_yt_container, YTSphere)
+@requires_file(landuse)
+def test_circle_lu():
+    ds = yt.load(landuse)
+    # make center, radius not even number of pixels
+    res = ds.resolution[0]
+    center = ds.domain_center
+    center[:2] += res / 3
+    radius = res * 1234.567
+    circle = ds.circle(center, radius)
 
-@requires_file(s2l2a_data)
-def test_circle_S2L2A():
-    ds = yt.load(s2l2a_data)
-    radius = ds.quan(500.,'m')
-    circle_centre = ds.arr([488012,6199162],'m')
-    circular_yt_container = ds.circle(circle_centre, radius)
-    circular_yt_container[('bands', 'S2_B02')]
-    assert isinstance(circular_yt_container, YTSphere)
+    # check number of points in sufficiently large circle
+    n1 = np.pi * circle.radius**2 / ds.resolution.prod()
+    n2 = circle[('bands', '200km_2p5m_N38E34_1')].size
+    breakpoint()
+    assert_almost_equal(n1/n2, 1, decimal=5)
+
+@requires_file(landsat)
+@requires_file(s2)
+def test_circle_ls():
+    fns = landsat_fns + s2_fns
+    ds = yt.load(*fns)
+    # make center, radius not even number of pixels
+    res = ds.resolution[0]
+    center = ds.domain_center
+    center[:2] += res / 3
+    radius = ds.domain_width[:2].min() * 0.25 + res / 2
+    circle = ds.circle(center, radius)
+
+    # check number of points in sufficiently large circle
+    n1 = np.pi * circle.radius**2 / ds.resolution.prod()
+    n2 = circle[('bands', 'LS_B1')].size
+    assert_almost_equal(n1/n2, 1, decimal=5)
+
+    n3 = circle[('bands', 'S2_B01')].size
+    assert_almost_equal(n1/n3, 1, decimal=5)
+
+    assert_equal(n2, n3)
+
+@requires_file(landsat)
+@requires_file(s2)
+def test_circle_s2():
+    fns = s2_fns + landsat_fns
+    ds = yt.load(*fns)
+    # make center, radius not even number of pixels
+    res = ds.resolution[0]
+    center = ds.domain_center
+    center[:2] += res / 3
+    radius = ds.domain_width[:2].min() * 0.25 + res / 2
+    circle = ds.circle(center, radius)
+
+    # check number of points in sufficiently large circle
+    n1 = np.pi * circle.radius**2 / ds.resolution.prod()
+    n2 = circle[('bands', 'LS_B1')].size
+
+    # lower decimal precision since this sphere is smaller
+    assert_almost_equal(n1/n2, 1, decimal=3)
+
+    n3 = circle[('bands', 'S2_B01')].size
+    assert_almost_equal(n1/n3, 1, decimal=3)
+
+    assert_equal(n2, n3)
