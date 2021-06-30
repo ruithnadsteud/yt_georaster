547c9bf603f87953a0ebe980884ba531c73920c4
Britton Smith <brittonsmith@gmail.com>
2021-06-30 16:54:48 +0100

    Black format conf.py.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,16 +15,17 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import glob
+
 # -- Project information -----------------------------------------------------
 
-project = 'yt_georaster'
-copyright = '2021, Team yt_georaster'
-author = 'Team yt_georaster'
+project = "yt_georaster"
+copyright = "2021, Team yt_georaster"
+author = "Team yt_georaster"
 
 # The short X.Y version.
-version = '1.0.0.dev1'
+version = "1.0.0.dev1"
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0.dev1'
+release = "1.0.0.dev1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,20 +34,21 @@ release = '1.0.0.dev1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
 ]
 
-intersphinx_mapping = \
-  {'yt': ('http://yt-project.org/docs/dev/', None),}
+intersphinx_mapping = {
+    "yt": ("http://yt-project.org/docs/dev/", None),
+}
 
 autosummary_generate = glob.glob("api_reference.rst")
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,7 +56,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -62,9 +64,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
